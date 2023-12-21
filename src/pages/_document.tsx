01273813b9e25ef45d74ed96d679f1b60601bb1b
Luis R. Carrillo <luisr_carrillo@outlook.com>
2023-12-20 23:06:30 -0600

    🔍  : Update Google Tag Manager key

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/no-danger */
 import { Head, Html, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
-const GA4_KEY = 'AW-11436772431';
+const GTM_KEY = 'GTM-TFNJPGKD';
 
 export default function Document() {
   return (
@@ -26,23 +27,23 @@ export default function Document() {
         <meta name='theme-color' content='#041535' />
         {/* Google Tag Manager */}
         <Script
-          id='ga4-script'
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA4_KEY}`}
-        />
-        <Script
-          id='ga4-html'
+          id='gtm-html'
           dangerouslySetInnerHTML={{
-            __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA4_KEY}');
-              `,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_KEY}');`,
           }}
         />
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_KEY}"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
